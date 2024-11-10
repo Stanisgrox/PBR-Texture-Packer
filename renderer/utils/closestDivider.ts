1c32aff14bf1fcb59d73af766c4c41cc678c81cc
Stanislav <orlandonetworks@gmail.com>
2024-11-10 05:38:55 +0300

    Prevented long cycle in thread counter (almost no perfomance gain in <4K)

--- renderer/utils/closestDivider.ts
+++ renderer/utils/closestDivider.ts
@@ -1,7 +1,9 @@
 export function closestDivider(n: number, start: number) {
 
-    for (start; start >= 1; start--) {
-        if (n % start === 0) return start;
+    for (let i = Math.min(start, Math.floor(Math.sqrt(n))); i >= 1; i--) {
+        if (n % i === 0) {
+            return i;
+        }
     }
 
     return 1;
